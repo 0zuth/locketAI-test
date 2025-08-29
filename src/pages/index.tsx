e897a05f46c67406025a266e2cc8d951c8239755
0zuth <breadjoo1014@naver.com>
2025-08-29 20:42:53 +0900

    design: 텍스트 추가 및 반응형 디자인 구현 (#2)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,28 @@
-import { Geist } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
-    >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl font-bold">Hello World</h1>
+    <div>
+      <h1 className="sr-only">제 1장 - 나의 사주팔자</h1>
+      <main className="max-w-md min-h-screen mx-auto">
+        <div className="relative w-full aspect-[375/2081] bg-saju-background">
+          <Image
+            src="/images/background.png"
+            alt="이미지"
+            fill
+            className="w-full h-full object-cover"
+            priority
+          />
+          <p className="absolute cursor-default text-center tracking-tight top-1/3 left-1/4 -translate-x-1/5 -translate-y-1/2 text-text-primary text-fluid-responsive">
+            이제 본격적으로 <br />
+            OO님의 사주팔자를 <br />
+            분석해볼 차례네요.
+          </p>
+          <p className="absolute cursor-default text-center tracking-tight top-1/2 right-2/5 -translate-y-1/3 text-text-primary text-fluid-responsive">
+            제가 oo님의 사주를 <br />
+            보기 쉽게 표로 정리했어요
+          </p>
+        </div>
       </main>
     </div>
   );
